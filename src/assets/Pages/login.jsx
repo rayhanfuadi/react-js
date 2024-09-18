7a105291cad62d7af7207daa3a96b129a16817fe
rayhanfuadi <oke.rayhan1@gmail.com>
2024-09-19 02:47:03 +0700

    test branch

--- src/assets/Pages/login.jsx
+++ src/assets/Pages/login.jsx
@@ -1,7 +1,7 @@
 const LoginPage = () => {
     return (
         <div className="">
-            <h1 className="text-3xl font-bold">Login Page</h1>
+            <h1 className="text-3xl font-bold">Login Page1</h1>
         </div>
     )
 }
