3ab018eebd140ccc2aa219107f57fb57d27c043c
rayhanfuadi <oke.rayhan1@gmail.com>
2024-09-23 17:42:03 +0700

    fix bug

--- src/components/Fragments/MenuBar.jsx
+++ src/components/Fragments/MenuBar.jsx
@@ -19,15 +19,24 @@ export const MenuBar = () => {
             <DropdownMenu className="flex justify-end w-full">
                 <DropdownMenuTrigger asChild className="border-none text-white">
                     <Button variant="outline" className="flex items-center gap-2">
-                        <img className="w-[20px] h-[20px] lg:w-[40px] lg:h-[40px]" src="img/pengguna.png" alt="" />
-                        <img className="w-[12px] h-[12px] lg:w-[24px] lg:h-[24px]" src=" img/arrowDown.svg" alt="" />
+                        <img className="w-[24px] h-[24px] lg:w-[40px] lg:h-[40px]" src="img/pengguna.png" alt="" />
+                        <img className="w-[24px] h-[24px] lg:w-[24px] lg:h-[24px]" src=" img/arrowDown.svg" alt="" />
                     </Button>
                 </DropdownMenuTrigger>
-                <DropdownMenuContent className="absolute -right-10 py-[8px] w-auto bg-primary text-white border-none">
-                    <DropdownMenuLabel className="px-[12px] py-[8px] hover:bg-[#0f1011] cursor-pointer">Profil Saya</DropdownMenuLabel>
-                    <DropdownMenuLabel className="px-[12px] py-[8px] hover:bg-[#0f1011] cursor-pointer">Ubah Premium</DropdownMenuLabel>
+                <DropdownMenuContent className="absolute  flex flex-col -right-10 py-[8px] w-auto bg-primary text-white border-none">
+                    <DropdownMenuLabel className="w-[150px] flex items-center gap-2 px-[12px] py-[8px] hover:bg-[#0f1011] hover:text-indigo-500 cursor-pointer">
+                        <img className="w-[20px]" src="img/user.svg" alt="" />
+                        Profil Saya
+                    </DropdownMenuLabel>
+                    <DropdownMenuLabel className="w-[150px] flex items-center gap-2 px-[12px] py-[8px] hover:bg-[#0f1011] hover:text-indigo-500 cursor-pointer">
+                        <img className="w-[20px]" src="img/star.svg" alt="" />
+                        Ubah Premium
+                    </DropdownMenuLabel>
                     <a href="/login">
-                        <DropdownMenuLabel className="px-[12px] py-[8px] hover:bg-[#0f1011] cursor-pointer">Keluar</DropdownMenuLabel>
+                        <DropdownMenuLabel className="w-[150px] flex items-center gap-2 px-[12px] py-[8px] hover:bg-[#0f1011] hover:text-indigo-500 cursor-pointer">
+                            <img className="w-[20px]" src="img/logout.svg" alt="" />
+                            Keluar
+                        </DropdownMenuLabel>
                     </a>
                 </DropdownMenuContent>
             </DropdownMenu>
