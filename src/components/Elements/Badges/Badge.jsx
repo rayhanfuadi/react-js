a877978d2688ad38508559bc2161912470d625ce
rayhanfuadi <oke.rayhan1@gmail.com>
2024-10-15 11:24:26 +0700

    test commit

--- src/components/Elements/Badges/Badge.jsx
+++ src/components/Elements/Badges/Badge.jsx
@@ -1,4 +1,4 @@
-const BadgeBiru = (props) => {
+const BadgeBiru = () => {
     return (
         <div
             className="absolute font-semibold text-white text-[10px] lg:text-[14px] px-[4px] py-[2px] mt-[8px] ml-[8px] lg:px-[10px] lg:py-[4px] bg-indigo-600 rounded-full lg:mt-[16px] lg:ml-[16px]">
