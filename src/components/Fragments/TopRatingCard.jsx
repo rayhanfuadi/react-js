60cd545e6ace84110eadd1e30768a38507d1775d
rayhanfuadi <oke.rayhan1@gmail.com>
2024-10-21 22:36:44 +0700

    update click: CardFilm

--- src/components/Fragments/TopRatingCard.jsx
+++ src/components/Fragments/TopRatingCard.jsx
@@ -4,7 +4,7 @@ const TopRatingCard = (props) => {
         <div className="swiper-slide w-full">
             <div className={`bg-indigo-50 rounded-xl relative flex justify-${justify} items-start`}>
                 {children}
-                <img className="w-full" src={img} alt="" />
+                <img className="w-full object-contain" src={img} alt="" />
             </div>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ export default {
   },
   plugins: [
     flowbite.plugin(),
+    require('daisyui'),
   ],
 }
 
